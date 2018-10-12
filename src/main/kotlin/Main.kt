151422a27b3024d33148e936a1a693ed9a6a5f64
Alberto <alberto@mendezcabrera.com>
2018-10-12 18:00:31 +0200

    Example created

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,11 @@
+import model.Place
+
 class Main {
     companion object {
         @JvmStatic
-        fun main(args : Array<String>) {
-            println("Hello, World!")
+        fun main(args: Array<String>) {
+            val place = Place("7700 3rd Street", "11419", "NY", "NY")
+            println(place.toString())
         }
     }
 }
--- src/main/kotlin/model/Place.kt
+++ src/main/kotlin/model/Place.kt
@@ -1,21 +1,13 @@
 package model
 
-import com.google.gson.JsonParser
-
 data class Place(
-        private val address: String = "",
-        private var cp: String = "",
-        val city: String = "",
-        val province: String = "") {
-    var location: Location = Location(lat, lng)
-    val lat: Double
-        get() {
-            return location.lat
-        }
-    val lng: Double
-        get() {
-            return location.lng
-        }
+        private val address: String,
+        private var cp: String,
+        val city: String,
+        val province: String,
+        val lat: Double = 0.0,
+        val lng: Double = 0.0) {
+    val location = Location(lat, lng)
 
     override fun toString(): String {
         return "Place: $address, $cp, $city ($province)\n\tLatitude: $lat\n\tLongitude: $lng"
