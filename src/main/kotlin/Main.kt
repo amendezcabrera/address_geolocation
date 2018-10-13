6db4b4a57c2921539e10af4e44dd6ce13175e4c4
Alberto <alberto@mendezcabrera.com>
2018-10-13 13:32:14 +0200

    Implemented to build the standalone jar file with gradle Catched a FileNotFoundException when opening the Excel file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 import helper.Parser
 import java.io.File
+import java.io.FileNotFoundException
 
 class Main {
     companion object {
@@ -13,8 +14,12 @@ class Main {
                 if (args.size > 1) {
                     delay = args[1].toLong()
                 }
-                val srcExcelFile = File(srcExcelFilePath)
-                Parser.Excel.parse(srcExcelFile, delay = delay)
+                try {
+                    val srcExcelFile = File(srcExcelFilePath)
+                    Parser.Excel.parse(srcExcelFile, delay = delay)
+                } catch (ex: FileNotFoundException) {
+                    printFileNotFound(srcExcelFilePath)
+                }
             } else {
                 printUsage()
             }
@@ -24,6 +29,10 @@ class Main {
             println("addressGeolocate EXCEL_FILE_PATH [DELAY_IN_MILLIS = 500]\n" +
                     "\t     EXCEL_FILE_PATH: Path to the Parser file with the addresses\n" +
                     "\t     DELAY_IN_MILLIS: Elapsed time to wait between API calls\n")
+        }
+
+        private fun printFileNotFound(path: String) {
+            println("File cannot be found in '$path'")
         }
     }
 }
